10fd3262115a67fddee6757927d3fb59566e1c3a
Ilia Azarashvili <ilia.azarashvili@gmail.com>
2022-07-22 22:16:03 +0400

    add json schema

---
+++ src/schemas/basket_schema.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, HttpUrl, UUID4
+
+
+class Data(BaseModel):
+    productId: int
+    productCount: int
+    price: float
+    title: str
+    imageUrl: HttpUrl
+    discountedPrice: float = None
+    discountedPercent: float = None
+    commercialTitle: str
+    quantity: int
+    isPreOrderProduct: str = None
+    productStatus: int
+    customerId: UUID4
+    details: str = None
+    discountType: int
+    discountValue: int = None
+    valueType: str = None
+    categorySlug: str
+    productSlug: str
+    productOriginalSlug: str
+
+
+class ProductDetail(BaseModel):
+    data: list[Data]
+    totalPrice: float
+    quantityFailed: bool
--- tests/basket_api/AdminBasket/test_admin_basket.py
+++ tests/basket_api/AdminBasket/test_admin_basket.py
@@ -1,8 +1,15 @@
+import json
 import os
+
+from jsonschema import validate
+from src.schemas.basket_schema import ProductDetail
+
+from src.utilities.response import Response
 
 from requests import Response
 from src.helpers.basket_helpers import ExtraBasketApi
 from src.utilities.checking_response import Checking
+
 
 user_id = os.getenv('USERID')
 
@@ -10,4 +17,10 @@ user_id = os.getenv('USERID')
 def test_admin_basket(get_admin_token):
     result_get: Response = ExtraBasketApi.admin_basket(get_admin_token, user_id)
     Checking.check_status_code(result_get, 200)
+    print(result_get.json())
+    name = ProductDetail.parse_obj(result_get.json())
+    print(name.totalPrice)
+
+
+
 
